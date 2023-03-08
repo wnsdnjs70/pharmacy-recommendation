ac5ab4519944dbec9be9ca0b20187769af1fc8f7
wnsdnjs70 <wnsdnjs700@naver.com>
2023-03-08 19:38:33 +0900

    first commit

---
+++ src/main/java/com/example/pharmacy_recommendation/PharmacyRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyRecommendationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pharmacy_recommendation/PharmacyRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
